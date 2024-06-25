471c51aa86b2bf1cf0f5a5e1eecca7f9c9c4cbe7
David Saidov <delightxxls@gmail.com>
2024-06-25 15:10:01 +0300

    upd urls

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='docs'),
-    path('api/auth/', include('djoser.urls')),
-    path('api/auth/', include('djoser.urls.authtoken')),
+    path('api/v1/auth/', include('djoser.urls')),
+    path('api/v1/auth/', include('djoser.urls.authtoken')),
 ]
