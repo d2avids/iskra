cbaa0ac15da153e5e06f6e84d1830d9d1b6f6688
Jivan-lvm <serezhkin.ivan@gmail.com>
2024-08-15 19:28:30 +0700

    del double

--- backend/users/constants.py
+++ backend/users/constants.py
@@ -2,22 +2,3 @@ PROFESSIONAL_COMPETENCES_VALIDATION_MSG = (
     'Профессиональные компетенции должны быть указаны в '
     'формате ["навык1", "навык2", "навык3"]'
 )
-
-
-TEST_RESULTS = [
-    '№',
-    'name',
-    'ERM',
-    'TMM',
-    'SS',
-    'ERS',
-    'TMS',
-    'EMP',
-    'ERI',
-    'PL',
-    'PRO',
-    'SF',
-    'IMP',
-    'NEU',
-    'CNTL'
-]
